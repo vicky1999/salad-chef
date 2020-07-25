e6e3cc7861d62393dc8c654cfa6ab65e888fd31c
vicky1999 <ravivignesh2000@gmail.com>
2020-07-25 16:23:41 +0530

    Player movement

---
+++ Assets/Scripts/Players.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players : MonoBehaviour
+{
+	public Transform player1,player2;
+	public float speed=3f;
+
+	void Start() {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float player1_hor=Input.GetAxis("Player1_Horizontal");
+    	float player1_vert=Input.GetAxis("Player1_Vertical");
+		
+		float player2_hor=Input.GetAxis("Player2_Horizontal");
+    	float player2_vert=Input.GetAxis("Player2_Vertical");
+		
+		player1.position=new Vector2(player1.position.x+player1_hor*speed*Time.deltaTime,
+			player1.position.y+player1_vert*speed*Time.deltaTime);
+        
+		player2.position=new Vector2(player2.position.x+player2_hor*speed*Time.deltaTime,
+			player2.position.y+player2_vert*speed*Time.deltaTime);
+        
+    }
+}
